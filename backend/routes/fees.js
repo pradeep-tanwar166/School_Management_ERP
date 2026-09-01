ecfa6baec00bf1f3b70137f69502b5e9e2cacc65
pradeeptanwar <pradeeptanwar166@gmail.com>
2026-09-01 04:41:42 -0700

    some mistakes in fees update

--- backend/routes/fees.js
+++ backend/routes/fees.js
@@ -1,6 +1,6 @@
 const express=require('express');
 const router=express.Router();
-const {fees,getfeesdata,deletefeesdata,updatefeesdata}=require("../controllers/feescontroller");
+const {fees,getfeesdata,updateFeesData,deletefeesdata}=require("../controllers/feescontroller");
 const {limiter}=require("../middleware/ratelimiter")
 
 
@@ -8,7 +8,7 @@ router.post("/fees",limiter,fees);
 
 router.get("/fees",limiter,getfeesdata)
 
-router.put("/fees/:id",updatefeesdata)
+router.put("/fees/:id",updateFeesData)
 
 router.delete("/fees/:id", deletefeesdata);
 
